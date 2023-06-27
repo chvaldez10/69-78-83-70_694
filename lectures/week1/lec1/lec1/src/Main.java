d78273fde35409df19e1d9069d4f2e986d9ebe3f
Christian Valdez <christianvaldez021@gmail.com>
2023-06-26 20:36:04 -0600

    added max func

--- lectures/week1/lec1/lec1/src/Main.java
+++ lectures/week1/lec1/lec1/src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        // add code
+        int[] arr = {1, 2, 3};
+        int result = maxNum(arr, arr.length);
+        System.out.println("max="+result);
+    }
+
+    private static int maxNum(int[] arr, int n) {
+        int max = arr[0];
+        int i = 1;
+        while (i < n){
+            if (max < arr[i]) {
+                max= arr[i];
+            }
+            i += 1;
+        }
+        return max;
     }
 }
