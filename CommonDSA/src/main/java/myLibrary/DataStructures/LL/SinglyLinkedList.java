5858cdf0ef75fa5569220af8030fe5e7fb7d01a6
chvaldez10 <christianvaldez021@gmail.com>
2023-08-16 10:51:54 -0600

    added linked list insert at position

--- CommonDSA/src/main/java/myLibrary/DataStructures/LL/SinglyLinkedList.java
+++ CommonDSA/src/main/java/myLibrary/DataStructures/LL/SinglyLinkedList.java
@@ -11,18 +11,66 @@ public class SinglyLinkedList {
         }
     }
 
+    // instant variables
     private Node head = null;
     private Node tail = null;
+    private int size = 0;
+
+    public int getSize() { return this.size; }
+
+    public int getHeadData() { return this.head.data; }
+    public int getTailData() { return this.tail.data; }
 
     /**
-     * Inserts a new node with the given data at the end of the list.
-     * @param data The data of the new node.
+     * Insert at the end of the list
+     * @param data data of the new node
      */
     public void insert(int data) {
         Node newNode = new Node(data);
         if(isEmpty()) head = newNode;
         else tail.next = newNode;
         tail = newNode;
+        size++;
+    }
+
+    /**
+     * Insert at the beginning of list
+     * @param data data of the new node
+     */
+    public void insertAtBeginning(int data){
+        Node newNode = new Node(data);
+        newNode.next = head;
+        head = newNode;
+        size++;
+    }
+
+    /**
+     * Inserts a new data after the first instance of value
+     * @param data data of the new node
+     * @param position index of the new node
+     */
+    public void insertAtPosition(int data, int position) {
+        if (position == 0) {
+            insertAtBeginning(data);
+            return;
+        }
+
+        Node currentNode = head;
+        int index = 0;
+
+        while (index != position-1 && currentNode != null) {
+            currentNode = currentNode.next;
+            index++;
+        }
+
+        if (currentNode == null) {
+            insert(data);
+        } else {
+            Node newNode = new Node(data);
+            newNode.next = currentNode.next;
+            currentNode.next = newNode;
+            size ++;
+        }
     }
 
     /**
@@ -30,4 +78,21 @@ public class SinglyLinkedList {
      * @return true if the list is empty, false otherwise.
      */
     public boolean isEmpty() { return head == null; }
+
+    /**
+     * Traverse linked list and print to terminal
+     */
+    public void display() {
+        if (isEmpty()) {
+            System.out.println("Linked List is Empty");
+            return;
+        }
+
+        Node current = head;
+        while ( current != null ){
+            System.out.print(current.data + " ");
+            current = current.next;
+        }
+        System.out.println();
+    }
 }
--- CommonDSA/src/test/java/myLibrary/DataStructures/LL/SLLTest.java
+++ CommonDSA/src/test/java/myLibrary/DataStructures/LL/SLLTest.java
@@ -1,15 +1,130 @@
 package myLibrary.DataStructures.LL;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class SLLTest {
+    private final ByteArrayOutputStream output = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+    @BeforeEach
+    public void setUpStreams() {
+        System.setOut(new PrintStream(output));
+    }
+    @AfterEach
+    public void restoreStreams() {
+        System.setOut(originalOut);
+    }
+    private int[] arr = {1, 2, 3, 4, 5};
+
     @Test
     public void testInsert(){
         SinglyLinkedList sll = new SinglyLinkedList();
-        assertTrue(sll.isEmpty()); // list is empty at the start
-        sll.insert(5); // add a Node
-        assertFalse(sll.isEmpty()); // list should not be empty
+
+        // list is empty at the start, size = 0
+        assertTrue(sll.isEmpty());
+        assertEquals(sll.getSize(), 0);
+
+        // adding a node, size = 1
+        sll.insert(5);
+        assertEquals(sll.getSize(), 1);
+        assertFalse(sll.isEmpty());
+    }
+
+    @Test
+    public void testInsertAtBeginning(){
+        SinglyLinkedList sll = new SinglyLinkedList();
+
+        // list is empty at the start, size = 0
+        assertTrue(sll.isEmpty());
+        assertEquals(sll.getSize(), 0);
+
+        // adding two nodes containing 2 and 3
+        sll.insert(2);
+        sll.insert(3);
+        assertEquals(sll.getHeadData(), 2);
+        assertEquals(sll.getTailData(), 3);
+        assertEquals(sll.getSize(), 2);
+        assertFalse(sll.isEmpty());
+
+        // insert at beginning
+        sll.insertAtBeginning(1);
+        assertEquals(sll.getHeadData(), 1);
+        assertEquals(sll.getTailData(), 3);
+        assertEquals(sll.getSize(), 3);
+    }
+
+        @Test
+        public void testTDisplay() {
+            String newLine = System.lineSeparator();
+            String expectedOutput = "1 2 3 4 5 " + newLine;
+            SinglyLinkedList sll = new SinglyLinkedList();
+
+            for (int i : arr ) sll.insert(i);
+            sll.display();
+            assertEquals(expectedOutput, output.toString());
+        }
+
+        @Test
+        public void testInsertAtPositionMiddle() {
+            SinglyLinkedList sll = new SinglyLinkedList();
+            for (int i : arr ) sll.insert(i);
+
+            String newLine = System.lineSeparator();
+            String expectedOutputMiddle = "1 2 3 6 4 5 " + newLine;
+
+            // head = 1, tail = 5, size = 5
+            assertEquals(sll.getHeadData(), 1);
+            assertEquals(sll.getTailData(), 5);
+            assertEquals(sll.getSize(), 5);
+
+            // insert after 3, 1 > 2 > 3 > 6 > 4 > 5
+            sll.insertAtPosition(6, 3);
+            sll.display();
+            assertEquals(expectedOutputMiddle, output.toString());
+        }
+
+    @Test
+    public void testInsertAtPositionBeginning() {
+        SinglyLinkedList sll = new SinglyLinkedList();
+        for (int i : arr ) sll.insert(i);
+
+        String newLine = System.lineSeparator();
+        String expectedOutputBeginning = "6 1 2 3 4 5 " + newLine;
+
+        // head = 1, tail = 5, size = 5
+        assertEquals(sll.getHeadData(), 1);
+        assertEquals(sll.getTailData(), 5);
+        assertEquals(sll.getSize(), 5);
+
+        // insert at beginning, 6 > 1 > 2 > 3  > 4 > 5
+        sll.insertAtPosition(6, 0);
+        sll.display();
+        assertEquals(expectedOutputBeginning, output.toString());
+    }
+
+
+    @Test
+    public void testInsertAtPositionEnd() {
+        SinglyLinkedList sll = new SinglyLinkedList();
+        for (int i : arr ) sll.insert(i);
+
+        String newLine = System.lineSeparator();
+        String expectedOutputBeginning = "1 2 3 4 5 6 " + newLine;
+
+        // head = 1, tail = 5, size = 5
+        assertEquals(sll.getHeadData(), 1);
+        assertEquals(sll.getTailData(), 5);
+        assertEquals(sll.getSize(), 5);
+
+        // insert at End, 1 > 2 > 3 > 4 > 5 > 6
+        sll.insertAtPosition(6, 5);
+        sll.display();
+        assertEquals(expectedOutputBeginning, output.toString());
     }
 }
