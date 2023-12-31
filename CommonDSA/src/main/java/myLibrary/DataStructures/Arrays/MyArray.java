7a36a84c642e80c031023523451dcc0baa2864d0
chvaldez10 <christianvaldez021@gmail.com>
2023-12-30 21:50:00 -0700

    added MyArray

---
+++ CommonDSA/src/main/java/myLibrary/DataStructures/Arrays/MyArray.java
@@ -0,0 +1,21 @@
+package myLibrary.DataStructures.Arrays;
+
+import java.util.ArrayList;
+
+public class MyArray {
+    private int length;
+    private ArrayList<Integer> data;
+
+    public MyArray() {
+        this.length = 0;
+        this.data = new ArrayList<>();
+    }
+
+    public int getArrayElement(int index) {
+        return this.data.get(index);
+    }
+
+    public void addElement(int data) {
+        this.data.add(data);
+    }
+}
---
+++ CommonDSA/src/test/java/myLibrary/DataStructures/Arrays/MyArrayTest.java
@@ -0,0 +1,25 @@
+package myLibrary.DataStructures.Arrays;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MyArrayTest {
+    private MyArray myArray;
+
+    @BeforeEach
+    void setUp() {
+        myArray = new MyArray();
+        myArray.addElement(1);
+        myArray.addElement(2);
+        myArray.addElement(3);
+    }
+
+    @Test
+    void testGetArrayElement(){
+        assertEquals(1, myArray.getArrayElement(0), "First element should be 1");
+        assertEquals(2, myArray.getArrayElement(1), "First element should be 2");
+        assertEquals(3, myArray.getArrayElement(2), "First element should be 3");
+    }
+}
