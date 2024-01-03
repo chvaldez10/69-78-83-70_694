1275fa7b1ae56d8f1b9e2dc380172304c73520d6
chvaldez10 <christianvaldez021@gmail.com>
2024-01-03 08:33:07 -0700

    added MyArray operations

--- CommonDSA/src/main/java/myLibrary/DataStructures/Arrays/MyArray.java
+++ CommonDSA/src/main/java/myLibrary/DataStructures/Arrays/MyArray.java
@@ -1,6 +1,7 @@
 package myLibrary.DataStructures.Arrays;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MyArray {
     private int length;
@@ -22,5 +23,22 @@ public class MyArray {
     public void addElement(int data) {
         this.data.add(data);
         this.length += 1;
+    }
+
+    public int removeLastElement() {
+        int lastElement = data.remove(length-1);
+        this.length--;
+        return lastElement;
+    }
+
+    public int removeElement(int index) {
+        int deletedElement = data.remove(index);
+        this.length--;
+        return deletedElement;
+    }
+
+    public void display(){
+        for (int d : data) System.out.print(d + " ");
+        System.out.println();
     }
 }
--- CommonDSA/src/test/java/myLibrary/DataStructures/Arrays/MyArrayTest.java
+++ CommonDSA/src/test/java/myLibrary/DataStructures/Arrays/MyArrayTest.java
@@ -1,12 +1,35 @@
 package myLibrary.DataStructures.Arrays;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MyArrayTest {
     private MyArray myArray;
+    private  final ByteArrayOutputStream output = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+    String newLine = System.lineSeparator();
+
+    @BeforeEach
+    public void setUpStreams() {
+        myArray = new MyArray();
+        System.setOut(new PrintStream(output));
+
+        myArray.addElement(1);
+        myArray.addElement(2);
+        myArray.addElement(3);
+    }
+
+    @AfterEach
+    public void restoreStreams() {
+        System.setOut(originalOut);
+    }
 
     @BeforeEach
     void setUp() {
@@ -22,5 +45,22 @@ class MyArrayTest {
         assertEquals(2, myArray.getArrayElement(1), "First element should be 2");
         assertEquals(3, myArray.getArrayElement(2), "First element should be 3");
         assertEquals(3, myArray.getArrayLength(), "The length of the array should be 3");
+    }
+
+    @Test
+    void testRemoveLastElement() {
+        assertEquals(3 , myArray.removeLastElement(), "The removed element should be 3.");
+        assertEquals(2, myArray.getArrayLength(), "The length of the array should now be 2.");
+    }
+
+    @Test
+    void testRemoveElement(){
+        String expectedOutput = "1 3 " + newLine;
+
+        assertEquals(2, myArray.removeElement(1), "The removed element should 2.");
+        assertEquals(2, myArray.getArrayLength(), "The length of the array should be 2.");
+
+        myArray.display();
+        assertEquals(expectedOutput, output.toString());
     }
 }
